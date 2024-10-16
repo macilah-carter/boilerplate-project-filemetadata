d5bfa4dab2052494ec0bcaa30164e22467fdd1e0
macilah-carter <museemacilah@gmail.com>
2024-10-16 18:51:16 +0300

    learning multer

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cors = require('cors');
+const mutler = require('multer');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +17,24 @@ app.get('/', function (req, res) {
 });
 
 
+const storage = mutler.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null,file.originalname)
+  }
+})
+
+const upload = mutler({storage: storage});
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
